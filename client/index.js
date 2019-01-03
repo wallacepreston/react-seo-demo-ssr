f6ea492d290d4cc33a18652cbdde270d8c179584
Preston Wallace <wallace.preston@gmail.com>
2019-01-03 15:43:13 -0800

    Bug Fix: Edited get route for homepage

--- client/index.js
+++ client/index.js
@@ -4,7 +4,6 @@ import {hydrate} from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux'
 
-// import '../public/index.css';
 import configureStore from './store'
 import Root from './components/root'
 
